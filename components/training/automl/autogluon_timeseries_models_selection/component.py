2b0bd7326babc88ed15032e3b00508701e349184
Dorota Laczak <dlaczak@redhat.com>
2026-04-07 13:22:22 +0200

    fix: Removed additional packages to install. All will be in base image
    Signed-off-by: Dorota Laczak <dlaczak@redhat.com>

--- components/training/automl/autogluon_timeseries_models_selection/component.py
+++ components/training/automl/autogluon_timeseries_models_selection/component.py
@@ -6,7 +6,6 @@ from kfp_components.utils.consts import AUTOML_IMAGE  # pyright: ignore[reportMi
 
 @dsl.component(
     base_image=AUTOML_IMAGE,  # noqa: E501
-    packages_to_install=["autogluon.timeseries==1.5.0"],
 )
 def autogluon_timeseries_models_selection(
     target: str,
