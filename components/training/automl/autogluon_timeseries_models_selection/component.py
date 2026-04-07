17f0271dfface5d93d340b949ee5d9959452cbf8
Dorota Laczak <dlaczak@redhat.com>
2026-04-07 13:29:33 +0200

    fix: Reduce TOP_N_MAX for timeseries
    Assisted by Cursor
    
    Signed-off-by: Dorota Laczak <dlaczak@redhat.com>

--- components/training/automl/autogluon_timeseries_models_selection/component.py
+++ components/training/automl/autogluon_timeseries_models_selection/component.py
@@ -61,7 +61,7 @@ def autogluon_timeseries_models_selection(
     DEFAULT_EVAL_METRIC = "MASE"
     DEFAULT_TIME_LIMIT = 600  # 10 minutes
 
-    TOP_N_MAX = 10
+    TOP_N_MAX = 7
 
     # Input validation
     for param, value in (
